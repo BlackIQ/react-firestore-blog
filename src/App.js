90cfe9dfa0835831efef9cd0c31ac00cf672062d
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-27 22:10:22 +0430

    Add route to auth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Navbar from "./components/navbar";
 import Home from "./pages/home";
+import Auth from "./pages/auth";
 import Blogs from "./pages/blogs";
 import BlogPage from "./components/blogPage";
 import New from "./pages/new";
@@ -15,6 +16,7 @@ function App() {
                     <Route exact path='/blogs'><Blogs/></Route>
                     <Route exact path='/new'><New/></Route>
                     <Route path='/blog/:id'><BlogPage/></Route>
+                    <Route exact path='/auth'><Auth /></Route>
                     <Route exact path='/home'><Home /></Route>
                 </Switch>
             </div>
