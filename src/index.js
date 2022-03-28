b750c46e432ad0dfe8a104e958aa4c25ba33b207
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-29 01:02:57 +0430

    Remove new

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 
 import 'mdb-ui-kit/css/mdb.min.css';
+import 'mdb-ui-kit/js/mdb.min.js';
 
 ReactDOM.render(
   <App />, document.getElementById('root')
